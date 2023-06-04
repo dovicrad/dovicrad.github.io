b23a0309fc1243df09241c721705e8f17e698a06
dovicrad <101804552+dovicrad@users.noreply.github.com>
2023-06-05 00:34:46 +0200

    Comment main.js

--- scripts/main.js
+++ scripts/main.js
@@ -9,12 +9,14 @@ const leaderboard = new Leaderboard()
 
 const game = new Minesweeper(modal, timer,leaderboard)
 
-// GET REQUIRED CONTAINERS
+// Getting containers
 const gameContainer = document.querySelector("#gameContainer")
 const gameElement = document.querySelector("#game")
 
 const form = document.getElementById("optionForm");
 
+
+// Handle size submit
 form.addEventListener("submit", function (event) {
     event.preventDefault()
     const selectedOption = document.querySelector('input[name="option"]:checked');
@@ -31,14 +33,18 @@ form.addEventListener("submit", function (event) {
             size = 30
             break
     }
-
+    
+    // Setup and display the game
     const bombs = Math.floor((size*size)/8)
     game.setup(bombs,size)
+    
     gameContainer.innerHTML = ""
+    
     gameContainer.appendChild(game.getElement())
     gameElement.appendChild(leaderboard.getElement(size))
     gameElement.appendChild(modal.getElement())
 
+    // Hide form display game
     document.querySelector("#sizeSelection").style.display = "none"
     gameElement.style.display = "block"
 })
